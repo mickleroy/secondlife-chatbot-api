44eca4039322ac8cb5c61b7f808e8b5a9607dbbb
Michael Leroy <michael.leroy@shinesolutions.com.au>
2025-03-22 19:59:03 +1100

    Initial commit

---
+++ test/secondlife-chatbot-api.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const SecondlifeChatbotApi = require('../lib/secondlife-chatbot-api-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/secondlife-chatbot-api-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new SecondlifeChatbotApi.SecondlifeChatbotApiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
